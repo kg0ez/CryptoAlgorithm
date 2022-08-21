0c1fd06e051e7ced16deeeb9cf06d287a6dff44a
kg0ez <82456927+kg0ez@users.noreply.github.com>
2022-08-21 20:56:48 +0300

    algorithm

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿using Algorithm;
+
+var lastInstrument = "USDT";
+int count = 1;
+
+CryptoAlgorithm crypto = new CryptoAlgorithm();
+
+List<string> chains = await crypto.GetChains(lastInstrument, count);
+
+Console.ReadLine();
